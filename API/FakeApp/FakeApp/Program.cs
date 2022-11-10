04597ac514ef6ad7f13c807a63a17b8db1aff453
Ian Jakalow <ijakalow@qw-corp.com>
2022-11-09 17:10:59 -0700

    Initial Checkin

---
+++ API/FakeApp/FakeApp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Runtime.InteropServices.ComTypes;
+
+namespace FakeApp 
+{
+    public class Program
+    {
+        public void Main(string[] args)
+        {
+            IMath math = new MathIsGreat();
+            IFile file = new FilesAreRad();
+            DoAllTheThings(file, math);
+        }
+
+        private void DoAllTheThings(IFile file, IMath math)
+        {
+            
+        }
+    }
+
+    public class MathIsGreat : IMath
+    {
+
+    }
+
+    public interface IMath
+    {
+    }
+
+    public class FilesAreRad : IFile
+    {
+
+    }
+    public interface IFile
+    {
+    }
+}
